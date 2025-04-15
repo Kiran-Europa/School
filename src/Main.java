de76394c8175c3ce21d267287cba3e8e2cf895c1
RC_Student_lab <kceuropa1@gmail.com>
2025-04-15 12:25:16 +0000

    first commitment:(

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        while(count< 5) {
+            JOptionPane.showInputDialog( null, "Enter number");
+
+            count++;
+
+
+
+        }
+    }
+}
